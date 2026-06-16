9944a2eac875094d525c2b3725ee1949a1b1280e
malannly <mamedovaanna.i@gmail.com>
2026-06-16 10:45:15 +0300

    modified config

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,10 +3,10 @@ import { DataSourceOptions } from 'typeorm';
 
 export const dbConfig = registerAs('db', () => ({
     type: 'postgres',
-    host: process.env.DB_HOST || 'aws-0-eu-west-1.pooler.supabase.com',
+    host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT ?? '5432', 10),
-    username: process.env.DB_USER || 'postgres.anlmwcgmlptkcxbbpdnx',
-    password: process.env.DB_PASSWORD || 'Wadik1996121413',
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'postgres'
 }))
 
