045d84d69266bc20f37f873c1f56cc9dea6a5c8e
malannly <mamedovaanna.i@gmail.com>
2026-07-12 21:50:51 +0300

    fixing the swagger cities

--- src/cities/dto/create-city.dto.ts
+++ src/cities/dto/create-city.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCityDto {
+
+  @ApiProperty({
+    example: 'Москва',
+  })
   @IsString()
   name: string;
 }
